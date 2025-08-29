fe7c2a3e983889586aaa080f3bafec2eb471e3e8
mailsonpuc <mailson.costa@proton.me>
2025-08-29 15:44:10 -0300

    refatora para Test

---
+++ LinuxApi.Test/CategoriaTest/GetCategoriaTest.cs
@@ -0,0 +1,21 @@
+using LinuxApi.Controllers;
+using LinuxApi.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+
+namespace LinuxApi.Test.CategoriaTest
+{
+    public class GetCategoriaTest
+    {
+
+        private readonly Mock<IUnitOfWork> _mockUof;
+        private readonly Mock<ILogger<CategoriasController>> _mockLogger;
+        private readonly CategoriasController _controller;
+
+        
+
+
+    }
+}
