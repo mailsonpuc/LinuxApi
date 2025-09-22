00673e90f578cb8bf91e8cce3295a38203b94fec
mailsonpuc <mailson.costa@proton.me>
2025-09-21 21:41:38 -0300

    authorize front

--- DistroFront/src/route.js
+++ DistroFront/src/route.js
@@ -9,28 +9,32 @@ import AdicionarDistro from './pages/AdicionarDistro';
 import AdicionarCategoria from './pages/AdicionarCategoria';
 import Login from './pages/Login';
 import Registrar from './pages/Registrar';
-
-//import Loading from './components/Loading';
-
+import PrivateRoute from './components/PrivateRoute'; // Componente que verifica autenticação
 
 function RoutesApp() {
     return (
         <BrowserRouter>
             <Header />
             <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='distros' element={<Distros />} />
-                <Route path='adddistro' element={<AdicionarDistro />} />
-                <Route path='addcategoria' element={<AdicionarCategoria />} />
-                <Route path='sobre' element={<Sobre />} />
-                <Route path='login' element={<Login />} />
-                <Route path='registrar' element={<Registrar />} />
-                {/* 
-                <Route path='loading' element={<Loading />} /> */}
+                {/* Rotas públicas */}
+                <Route path="login" element={<Login />} />
+                <Route path="registrar" element={<Registrar />} />
+
+                {/* Rota privada com layout de rotas protegidas */}
+                <Route element={<PrivateRoute />}>
+                    {/* Todas as rotas aqui serão protegidas */}
+                    <Route path="/" element={<Home />}>
+                        {/* Rotas internas */}
+                        <Route path="distros" element={<Distros />} />
+                        <Route path="adddistro" element={<AdicionarDistro />} />
+                        <Route path="addcategoria" element={<AdicionarCategoria />} />
+                        <Route path="sobre" element={<Sobre />} />
+                    </Route>
+                </Route>
 
             </Routes>
         </BrowserRouter>
-    )
+    );
 }
 
 export default RoutesApp;
