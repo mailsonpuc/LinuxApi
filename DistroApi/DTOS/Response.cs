511596390e8faf12a2164c88081caeccaca85e76
mailsonpuc <mailson.costa@proton.me>
2026-01-10 20:27:41 -0300

    Delete DistroApi directory

--- DistroApi/DTOS/Response.cs
+++
@@ -1,9 +0,0 @@
-
-namespace DistroApi.DTOS
-{
-    public class Response
-    {
-        public string? Status { get; set; }
-        public string? Message { get; set; }
-    }
-}
--- DistroApi/DTOS/TokenModel.cs
+++
@@ -1,9 +0,0 @@
-
-namespace DistroApi.DTOS
-{
-    public class TokenModel
-    {
-        public string? AccessToken { get; set; }
-        public string? RefreshToken { get; set; }
-    }
-}
