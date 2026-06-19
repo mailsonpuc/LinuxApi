3a7d13c6842ed97aa00db11a3255f0ce6015c212
mailsonpuc <mailson.costa@proton.me>
2026-06-19 13:58:12 -0300

    usando o modelo qwen2.5:3b

--- DistroBackEnd/Distro.API/DTOs/GenerateRequestDTO.cs
+++ DistroBackEnd/Distro.API/DTOs/GenerateRequestDTO.cs
@@ -5,7 +5,7 @@ namespace Distro.API.DTOs
     public class GenerateRequestDTO
     {
         [Required]
-        public string? Model { get; set; }
+        public string? Model { get; set; } //usando o qwen2.5:3b
 
         [Required]
         public string? Prompt { get; set; }
